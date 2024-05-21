4c9f5dce03c2dd9a83d3e378c3a8cf2b1f65489e
asifbuet047 <asifbuet047@gmail.com>
2024-05-22 03:35:50 +0600

    Create a express app that running on specific port and successful response from espress app to client

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-const a: string = "dasd";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+//our express app
+const app: Application = express();
+
+//express app middlewares like cors, json parser
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  console.log(req.params);
+  res.json({
+    success: true,
+    message: "Connection successful",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `ph-level-2-apollo-batch-3-assignment-2 server is running on port ${process.env.PORT}`,
+  );
+});
+
+export default app;
